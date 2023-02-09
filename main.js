64d3da1106dc500f488f9ce5789d6244698ab5f9
NicolasEiriz <eiriznicolas@gmail.com>
2023-02-09 11:22:18 -0300

    add some style

--- main.js
+++ main.js
@@ -32,95 +32,88 @@ function divide(n1,n2){
 /*Create a new function operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.*/
 
 function operate(operator, n1, n2){
- return operator(n1,n2)
+  if(operator == '+'){
+    return add(n1,n2)
+  }
+    else if(operator == '-'){
+      return subtract(n1,n2)
+    }
+      else if(operator == '/'){
+        return divide(n1,n2)
+      } 
+        else if(operator == '*'){
+          return multiply(n1,n2)
+        }
+ 
 }
 
 /*Create the functions that populate the display when you click the number buttons. You should be storing the ‘display value’ in a variable somewhere for use in the next step.*/
 
+//Set a variable display with a global scope, this variable will hold the current number o numbers selection
+
+let display = document.querySelector('#display')
+let displayValue = ''
+
 //holds the buttons which have a number in a variable, node type
 
-let buttons = document.querySelectorAll('.button')
-console.log(buttons);
+let Numberbuttons = document.querySelectorAll('.number')
+console.log(Numberbuttons);
 
 //add an event listener for Each buttons 
 
-buttons.forEach(button=>{
-button.addEventListener('click', populateDisplay)
+Numberbuttons.forEach(button=>{
+button.addEventListener('click', (e)=>{
+  display.textContent += e.target.textContent
+  displayValue = display.textContent
+})
 })
 
+//You’ll need to store the first number that is input into the calculator when a user presses an operator
 
-function callCalculator(e){
-  let operatorValue = e.target.textContent
-  console.log(operatorValue)
-}
+//and also save which operation has been chosen and then operate() on them when the user presses the “=” key.
 
-
-
-//Set a variable display with a global scope, this variable will hold the current number o numbers selection
-
-let display = ''
-
-let operator
-
-/*Create the functions that populate the display when you click the number buttons*/
-
-function populateDisplay(e){
- display += e.target.textContent  
-}
-
-/*Make the calculator work! You’ll need to store the first number that is input into the calculator when a user presses an operator, and also save which operation has been chosen and then operate() on them when the user presses the “=” key.*/
+let firstNumber
+let operatorSelected
 
 
 let operators = document.querySelectorAll('.operator')
-console.log(operators);
+
 
 operators.forEach(operator=>{
-  operator.addEventListener('click', storeFirstNum)
+  operator.addEventListener('click', (e)=>{
+    
+    firstNumber = Number(displayValue)
+    operatorSelected = e.target.textContent
+    console.log(firstNumber, operatorSelected)
+    display.textContent = '';
+  })
 })
 
-// store the first number that is input into the calculator when a user presses an operator,
+//save which operation has been chosen and then operate() on them when the user presses the “=” key.
 
-let firstNumber
-let operatorValue
-let result 
+let equalOperator = document.querySelector('.equal')
 
-let h2 = document.querySelector('h2')
 
-function storeFirstNum(e){
-  firstNumber = Number(display)
-  operatorValue = e.target.textContent
-  display = ''
+
+equalOperator.addEventListener('click', ()=>{
+  const result = operate(operatorSelected, firstNumber, Number(display.textContent))
+
+  populateDisplay(`${firstNumber} ${operatorSelected} ${display.textContent} = ${result}`)
+  document.querySelector('.message').classList.toggle('toogle')
+})
+
+
+/*Create the functions that populate the display when you click the number buttons*/
+
+function populateDisplay(num){
+  display.textContent = num
+
 }
 
-let equalSign = document.querySelector('.equal')
 
-equalSign.addEventListener('click', operate)
-
-function operate(){
+document.querySelector('.clear').addEventListener('click', ()=>{
+  document.querySelector('.message').classList.toggle('toogle')
+  firstNumber = 0
+  display.textContent = ''
   
-  if(operatorValue === '+'){
-    result = add(firstNumber, Number(display))
-  } 
-    else if(operatorValue === '-'){
-      result = subtract(firstNumber, Number(display))
-    } 
-      else if(operatorValue === '/'){
-        result = display(firstNumber, Number(display))
-      } else if(operatorValue === '*'){
-        result =  multiply(firstNumber, Number(display))
-      } else if(operator === 'clear'){
-        display = ''
-        firstNumber = 0
-      }
-
-      h2.textContent = result
-      display = ''
-      firstNumber = 0
-}
-
-
-
-
-/*You should already have the code that can populate the display, so once operate() has been called, update the display with the ‘solution’ to the operation.
-This is the hardest part of the project. You need to figure out how to store all the values and call the operate function with them. Don’t feel bad if it takes you a while to figure out the logic.
-*/
+})
